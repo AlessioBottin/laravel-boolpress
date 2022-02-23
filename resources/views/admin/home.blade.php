c272e948c5a6f74fdc58a246ac010b912917e604
AlessioBottin <alessiobottin2002@gmail.com>
2022-02-23 16:34:48 +0100

    added user_info table and info in home view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Ciao {{ Auth::user()->name }}, benvenuto nell'area per gli admin</h1>
+    <h1>Ciao {{ $user->name }}, benvenuto nell'area per gli admin</h1>
+
+    @if ($userInfo)
+        <div>Telefono: {{ $userInfo->phone }}</div>
+        <div>Indirizzo: {{ $userInfo->address }}</div>
+    @endif
 @endsection
